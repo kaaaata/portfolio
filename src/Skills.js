1866c4db4d7daf18d3d107ee3dd8bd32c27dffab
diamond <diamond@Catherines-MacBook-Pro.local>
2018-02-26 13:49:15 -0800

    Design skills section

--- src/Skills.js
+++ src/Skills.js
@@ -2,10 +2,46 @@ import React, { Component } from 'react';
 import './Skills.css';
 
 export default class Skills extends Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        [
+          { skill: 'HTML', image: 'html.png', specs: ['HTML5'] },
+          { skill: 'CSS', image: 'css.png', specs: ['CSS3'] },
+          { skill: 'JavaScript', image: 'js.png', specs: ['ES7', 'NodeJS', 'JSX'] },
+        ],
+        [
+          { skill: 'React', image: 'react.png', specs: ['React 16', 'React Router 4'] },
+          { skill: 'SQL', image: 'react.png', specs: ['PostgreSQL'] },
+          { skill: 'Git', image: 'react.png', specs: ['GitHub', 'Git Command Line'] },
+        ],
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="skills">
-        <div>Skills</div>
+        <div className="skills-title">SKILLS</div>
+        {this.state.skills.map((three, index) => (
+          <div key={index} className="skills-row">
+            {three.map((skill, index) => (
+              <div key={index} className="skill">
+                <div className="skill-left">
+                  <div className="skill-image" style={{ backgroundImage: `url(${require(`./assets/${skill.image}`)})` }} />
+                </div>
+                <div className="skill-right">
+                  <div className="skill-specs">
+                    {skill.specs.map((spec, index) => (
+                      <div key={index} className="skill-spec">{spec}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
