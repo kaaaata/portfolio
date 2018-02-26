1910b558e52af20e6ae1b9b5f8275038eec9d6e7
diamond <diamond@catherines-mbp.attlocal.net>
2018-02-26 00:21:08 -0800

    Add 4 header-media-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Header from './Header';
+// import Landing from './Landing';
+// import About from './About';
+// import Projects from './Projects';
+// import Skills from './Skills';
+// import Contact from './Contact';
+// import Header from './Header';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        Hello World!
+        <Header />
+        {/* <Landing />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer /> */}
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {  
+  render() {
+    return (
+      <div className="header">
+        {[
+          { image: 'github.png', url: 'https://github.com/kaaaata' },
+          { image: 'linkedin.png', url: 'https://www.linkedin.com/in/kaaaata/' },
+          { image: 'facebook.png', url: 'https://www.facebook.com/blueconiferforest' },
+          { image: 'dota.png', url: 'https://www.dotabuff.com/players/125258124' },
+        ].map((media, index) => (
+          <div key={index}>
+            <a href={media.url}>
+              <div className="header-media-icon" style={{ backgroundImage: `url(${require(`./assets/${media.image}`)})` }} />
+            </a>
+          </div>
+        ))}        
+      </div>
+    );
+  }
+}
