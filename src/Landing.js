1df9f200a8435044c55bf25d8b2bcc4b62bdc90d
diamond <diamond@catherines-mbp.attlocal.net>
2018-03-05 01:14:30 -0800

    Add low-med-high landing image

--- src/Landing.js
+++ src/Landing.js
@@ -5,9 +5,14 @@ export default class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <br /><br /><br /><br /><br />
-        <div className="landing-name">CATHERINE HAN</div>
-        <div className="landing-title">FRONT END ENGINEER</div>
+        <div className="landing-background landing-background-low" />
+        <div className="landing-background landing-background-med" />
+        <div className="landing-background landing-background-high" />
+        <div className="landing-content">      
+          <br /><br /><br /><br /><br />
+          <div className="landing-name">CATHERINE HAN</div>
+          <div className="landing-title">FRONT END ENGINEER</div>
+        </div>
       </div>
     );
   }
