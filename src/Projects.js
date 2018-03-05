c8c5e77893cff206317d1eb8febd4d9fe21ff563
diamond <diamond@catherines-mbp.attlocal.net>
2018-03-04 23:08:50 -0800

    Touch up projects text

--- src/Projects.js
+++ src/Projects.js
@@ -10,19 +10,19 @@ export default class Projects extends Component {
         { name: 'THE HAPPINESS PROJECT', image: 'thp.png', shade: 'yellow',
           github: 'https://github.com/kaaaata/the-happiness-project', url: 'https://the-happiness-project.herokuapp.com',
           tools: ['HTML', 'CSS', 'JavaScript', 'React', 'Lo-Dash'],
-          blurb: 'The Happiness Project shares stories of what makes people happy. Currently under construction.' },
+          blurb: 'The Happiness Project shares stories of what makes people happy. Under construction.' },
         { name: 'JELLY', image: 'jelly.png', shade: 'blue',
           github: 'https://github.com/kaaaata/jelly', url: 'https://jellyfishapp.herokuapp.com/',
           tools: ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'ExpressJS'],
-          blurb: 'Jelly is a simple web app - a customizable terminal for browsing the web. Create your own commands :)' },
+          blurb: 'Jelly is a simple customizable terminal for browsing the web.' },
         { name: 'BIG 2', image: 'big2.png', shade: 'green',
           github: 'https://github.com/kaaaata/big-2', url: 'https://big2app.herokuapp.com',
           tools: ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'ExpressJS', 'Lo-Dash'],
-          blurb: 'Play against the AI in this Big 2 clone built with plain HTML/CSS/JS. Currently working on multiplayer.' },
-        { name: 'UBER BACKEND CLONE', image: 'uber.jpg', shade: 'black',
+          blurb: 'Play against the AI in this Big 2 clone. Multiplayer coming soon.' },
+        { name: 'QUIKKER', image: 'uber.jpg', shade: 'black',
           github: 'https://github.com/kaaaata/thesis', url: null,
           tools: ['JavaScript', 'ExpressJS', 'Mocha', 'New Relic', 'Siege', 'Lo-Dash'],
-          blurb: 'This project is a clone of Uber\'s backend matching microservice API.' },
+          blurb: 'Quikker is a backend clone of Uber\'s matching microservice API.' },
         { name: 'PORTFOLIO', image: 'portfolio.png', shade: 'purple',
           github: 'https://github.com/kaaaata/portfolio', url: 'http://www.kaaaata.com',
           tools: ['HTML', 'CSS', 'JavaScript', 'React', 'Lo-Dash'],
@@ -30,7 +30,7 @@ export default class Projects extends Component {
         { name: 'XELP', image: 'xelp.png', shade: 'orange',
           github: 'https://github.com/kaaaata/yelp1', url: null,
           tools: ['HTML', 'CSS', 'JavaScript', 'React', 'Travis CI', 'Lo-Dash'],
-          blurb: 'Xelp is a fullstack Yelp clone I built with a group of four. I designed the search feature, front-end and back-end.'}
+          blurb: 'Xelp is a fullstack Yelp clone. I worked on the search feature.'}
       ],
       projectsPerRow: window.innerWidth >= 1100 ? 3 : (window.innerWidth >= 700 ? 2 : 1),
     };
@@ -71,13 +71,6 @@ export default class Projects extends Component {
                     <a href={project.github} target="_blank">
                       <span className="project-link"><u>GitHub</u></span>
                     </a>
-                    {project.url &&
-                      <span>&nbsp;|&nbsp; 
-                        <a href={project.url} target="_blank">
-                          <span className="project-link"><u>Website</u></span>
-                        </a>
-                      </span>
-                    }
                   </div>
                   <div className="project-tools">
                     {project.tools.map((tool, index) => (
@@ -97,5 +90,3 @@ export default class Projects extends Component {
     );
   }
 }
-
-
