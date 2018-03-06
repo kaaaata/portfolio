7302f2e07da7645c2f7eb8b69e2e3ac5789c43da
diamond <diamond@catherines-mbp.attlocal.net>
2018-03-06 02:59:32 -0800

    sassify

--- src/About.js
+++ src/About.js
@@ -5,12 +5,12 @@ export default class About extends Component {
   render() {
     return (
       <div className="about">
-        <div><div className="about-content-image" /></div>
-        <div className="about-content">
-          <div className="about-blurb">
+        <div><div className="image" /></div>
+        <div className="content">
+          <div className="blurb">
             Hi, my name is Catherine, and I build web apps with JavaScript.
           </div>
-          <div className="about-content-text">
+          <div className="text">
             Originally a Finance major, I was introduced to coding with Python and Excel VBA scripting. I was immediately drawn in, fascinated by how code could automate boring tasks, and create cool small programs. The area that I fell most deeply in love with, though, was front end web development. Now, I'm focusing my energy on becoming the best front end engineer I can be, learning new skills and working on projects daily.
           </div>
         </div>
