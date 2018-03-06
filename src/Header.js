7d901065a3d5b5eeecfaf199c91003d09a328ea6
diamond <diamond@catherines-mbp.attlocal.net>
2018-03-06 02:45:05 -0800

    Install SASS, Refactor header with SASS

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,16 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="header-background" />
-        <div className="header-foreground">
-          <div className="header-left">
+        <div className="background" />
+        <div className="foreground">
+          <div className="left">
             {['Home', 'About', 'Projects', 'Skills', 'Contact'].map((scroller, index) => (
-              <div key={index} className="header-scroller" onClick={() => this.props.scrollToRef(scroller.toLowerCase())}>
+              <div key={index} className="scroller" onClick={() => this.props.scrollToRef(scroller.toLowerCase())}>
                 {scroller.toUpperCase()}
               </div>
             ))}
           </div>
-          <div className="header-right">
+          <div className="right">
             {[
               { image: 'github.png', url: 'https://github.com/kaaaata' },
               { image: 'linkedin.png', url: 'https://www.linkedin.com/in/kaaaata/' },
@@ -23,7 +23,7 @@ export default class Header extends Component {
             ].map((media, index) => (
               <div key={index}>
                 <a href={media.url} target="_blank">
-                  <div className="header-media-icon" style={{ backgroundImage: `url(${require(`./assets/${media.image}`)})` }} />
+                  <div className="media-icon" style={{ backgroundImage: `url(${require(`./assets/${media.image}`)})` }} />
                 </a>
               </div>
             ))}        
