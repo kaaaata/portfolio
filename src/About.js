ecfb61ebd194c3f5f539199104c4094c112e07d1
diamond <diamond@catherines-mbp.attlocal.net>
2018-05-03 04:03:11 -0700

    Update big-2 and add Django

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ export default class About extends Component {
             Hi, my name is Catherine, and I build apps for the web.
           </div>
           <div className="text">
-            Originally a Finance major, I was introduced to coding with scripting in Python and Excel VBA. I was immediately drawn in, fascinated by how code could automate work, and bring ideas to life on the screen. The area that I fell most deeply in love with, though, was web development. Now, I'm focusing my energy on becoming the best software engineer I can be, learning new skills and working on projects daily.
+            Originally a Finance major, I was introduced to coding with scripting in Python and Excel VBA. I was immediately drawn in, fascinated by how code could automate work, and bring ideas to life on the screen. The area that I fell most deeply in love with, though, was fullstack web development. Now, I'm focusing my energy on becoming the best software engineer I can be, learning new skills and working on projects daily.
           </div>
         </div>
       </div>
