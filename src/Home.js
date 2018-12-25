19a749f1af98ae7e4052ba2c3d35f08e3660b933
kaaaata <catherinehan714@gmail.com>
2018-12-25 02:56:29 -0800

    Add emotion library for css-in-js

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,25 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-// import './styles/Home.css';
+import { css, jsx } from '@emotion/core'; /** @jsx jsx */
 
+const color = '';
+const emotionCss = css`
+  background-color: hotpink;
+  &:hover {
+    color: ${color};
+  }
+`;
 const Home = () => (
   <section>
     home page!
     <Link to='/about'>
       go to another page
     </Link>
+
+    <div
+      css={emotionCss}
+    >
+      This has a hotpink background.
+    </div>
   </section>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import store from './stores/store';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
