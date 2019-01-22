6f52e6cfd9582dd618f7e16a1c6cfe549d8bcef0
kaaaata <catherinehan714@gmail.com>
2019-01-22 02:33:28 -0800

    Edit some copy

--- src/Skills.js
+++ src/Skills.js
@@ -8,41 +8,41 @@ import { trackStats } from './utils/graphql';
 const skills = [
   {
     name: 'React',
-    description: 'I\'ve used React extensively at work and in side projects. Probably 50% of my total time spent coding went into React!',
+    description: 'I\'ve used React extensively at work and in side projects. Probably 50% of my total time spent coding right now is React...',
     flavorTexts: ['Familiar with React 15/16', 'Fragments FTW.']
   },
   {
     name: 'Redux',
     description: 'Redux is a flux state container I use in my side projects. Hoping to gain professional experience with it at some point!',
-    flavorTexts: ['This site has functional Redux architecture built into it...', '...but is currently too small to make any sensible use of it!']
+    flavorTexts: ['This site uses Redux lightly']
   },
   {
     name: 'Fluxible',
-    description: 'Fluxible is a flux state container tech I used in conjunction with React at Massdrop. It\'s similar to Redux, but uses handlers to store data in multiple stores instead of reducing to a single store.',
+    description: 'Fluxible is a flux state container tech I use in conjunction with React at Massdrop. It\'s similar to Redux, but uses handlers to store data in multiple stores instead of reducing to a single store.',
     flavorTexts: ['I\'ve worked on one of largest repos out there that uses Fluxible!']
   },
   {
     name: 'Git',
     description: 'I use Git to manage all my projects. Having experimented with various Git softwares, I still prefer the command line...',
-    flavorTexts: ['The most useful command: git symbolic-ref --short -q HEAD']
+    flavorTexts: ['The most useful command: git symbolic-ref --short -q HEAD', 'GitKraken is cool']
   },
   {
     name: 'Node',
-    description: 'On the backend, Node is my most comfortable language. I have experience working in Express, and have also experimented with GraphQL and Koa.'
+    description: 'On the backend, Node is my most comfortable environment. I have experience working in Express, and have also experimented with GraphQL.'
   },
   {
     name: 'GraphQL',
     description: 'I\'ve only recently picked up GraphQL, but it has been a blast working with it so far. I aspire at some point to gain professional experience working with GraphQL on a large scale application.',
-    flavorTexts: ['This website utilizes GraphQL!']
+    flavorTexts: ['This website is GraphQL powered!']
   },
   {
     name: 'Sass',
-    description: 'SASS/SCSS are style sheet languages I\'ve used both professionally and in side projects. They are very cool, especially Sass with its pimpy syntax, but CSS-in-JS is on another level.'
+    description: 'SASS/SCSS are style sheet languages I\'ve used both professionally and in side projects. They are very cool, especially Sass with its syntax, but CSS-in-JS is on another level.'
   },
   {
     name: 'Emotion',
-    description: 'Emotion is a new CSS-in-JS library that is lightweight, powerful, and fast. It combines JavaScript and SCSS to allow users to write SCSS as JS template literals, allowing for WILD dynamic styling possibilities and code reusability.',
-    flavorTexts: ['100% of this site\'s CSS is from Emotion!']
+    description: 'Emotion is a new CSS-in-JS library that is lightweight, powerful, and fast. It combines JavaScript and SCSS to allow users to write SCSS as JS template literals.',
+    flavorTexts: ['100% of this site\'s CSS is in Emotion!', 'I want to learn more about effective CSS-in-JS code patterns']
   },
   {
     name: 'Bash',
@@ -66,11 +66,11 @@ const skills = [
   },
   {
     name: 'Postgres',
-    description: 'Postgres is my database tech of choice for side projects. Using the Knex library to interface server-side JavaScript with Postgres (and also for migrations), working with this tech has been wonderful. Additionally, Heroku has a great Postgres add-on, allowing for easy deployment of Postgres-powered web apps.'
+    description: 'Postgres is my database tech of choice for side projects. I use it with the Knex library, and overall working with them has been wonderful. Additionally, Heroku\'s got a great Postgres add-on, allowing for easy database deployment.'
   },
   {
     name: 'Heroku',
-    description: 'Heroku is a cloud platform that enables easy deployment of both full stack and serverless web apps. I\'ve deployed many different projects to Heroku.',
+    description: 'Heroku is a cloud platform that enables easy deployment of both full stack and serverless web apps. I\'ve deployed many different projects to Heroku (with only mild amounts of headache....)',
     flavorTexts: ['This site is hosted on Heroku!']
   },
 ];
