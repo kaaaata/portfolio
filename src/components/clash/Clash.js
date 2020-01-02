b109a841c8e7b9f7c15e599a056b63b208a7ee4f
kaaaata <catherinehan714@gmail.com>
2020-01-02 15:35:44 -0800

    Refactor functional component to class

--- src/components/clash/Clash.js
+++ src/components/clash/Clash.js
@@ -1,7 +1,6 @@
-import { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { css, jsx } from '@emotion/core'; /** @jsx jsx */
-import useInterval from '../utils/useInterval';
-import deckUtils from './deckUtils';
+import { ArrayOfCards } from './arrayOfCards';
 import { Spacer } from '../particles';
 import { Card } from './Card';
 import {
@@ -19,71 +18,100 @@ const clashCss = css`
   height: 600px;
   position: relative;
   background: #B2967D;
+
+  .enemy_side, .your_side {
+    perspective: 1600px;
+  }
 `;
 
-export const Clash = () => {
-  const [yourDeck, setYourDeck] = useState([
-    ...Array(15).fill(cards['Strike']).map(card => ({ ...card, player: 'you' })),
-    { ...cards['Healing Potion'], player: 'you' }
-  ]);
-  const [yourDiscard, setYourDiscard] = useState([
-    ...Array(3).fill(cards['Strike']).map(card => ({ ...card, player: 'you' })),
-  ]);
-  const [yourBanish, setYourBanish] = useState([]);
-  const [enemyDeck, setEnemyDeck] = useState([
-    ...Array(13).fill(cards['Strike']),
-    cards['Healing Potion'],
-    ...Array(3).fill(cards['Strike'])
-  ].map(card => ({ ...card, player: 'opponent' })));
-  const [enemyDiscard, setEnemyDiscard] = useState([]);
-  const [enemyBanish, setEnemyBanish] = useState([]);
-  const [yourHand, setYourHand] = useState([]);
+export class Clash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yourDeck: [
+        ...Array(15).fill(cards['Strike']).map(card => ({ ...card, player: 'you' })),
+        { ...cards['Healing Potion'], player: 'you' }
+      ],
+      yourDiscard: [
+        ...Array(3).fill(cards['Strike']).map(card => ({ ...card, player: 'you' })),
+      ],
+      yourBanish: [],
+      enemyDeck: [
+        ...Array(13).fill(cards['Strike']),
+        cards['Healing Potion'],
+        ...Array(3).fill(cards['Strike'])
+      ].map(card => ({ ...card, player: 'opponent' })),
+      enemyDiscard: [],
+      enemyBanish: [],
 
-  const [isPlayersTurn, setIsPlayersTurn] = useState(true);
-  const [isAnimating, setIsAnimating] = useState(false);
+      yourHand: [],
+      stack: [],
 
-  let interval;
+      isPlayersTurn: true,
+      isAnimating: false
+    };
 
-  useEffect(() => {
-    setYourHand(yourDeck.slice(yourDeck.length - 3, yourDeck.length));
-    setYourDeck(yourDeck.slice(0, yourDeck.length - 3));
-  }, []);
+    this.interval = []; // only have one active interval, for simplicity/perf
+  }
 
-  const playCard = (card, isPlayersTurn) => {
+  componentDidMount() {
+    const { yourDeck } = this.state;
+    this.setState({
+      yourHand: yourDeck.slice(yourDeck.length - 3, yourDeck.length),
+      yourDeck: yourDeck.slice(0, yourDeck.length - 3)
+    });
+  }
+
+  playCard(card) {
+    const {
+      isAnimating,
+      isPlayersTurn
+    } = this.state;
+
     if (isAnimating || !isPlayersTurn) {
       return;
-    } else {
-      setIsAnimating(true);
     }
 
-    console.log('playing card', card);
+    const playerStateKey = card.player === 'you' ? 'your' : 'enemy';
+    const opponentStateKey = card.player === 'you' ? 'enemy' : 'your';
+    const stateKeys = {
+      player: {
+        deck: `${playerStateKey}Deck`,
+        discard: `${playerStateKey}Discard`,
+        banish: `${playerStateKey}Banish`,
+      },
+      opponent: {
+        deck: `${opponentStateKey}Deck`,
+        discard: `${opponentStateKey}Discard`,
+        banish: `${opponentStateKey}Banish`,
+      }
+    };
+    const stateCopy = {
+      player: {
+        deck: new ArrayOfCards([...this.state[stateKeys.player.deck]]),
+        discard: new ArrayOfCards([...this.state[stateKeys.player.discard]]),
+        banish: new ArrayOfCards([...this.state[stateKeys.player.banish]])
+      },
+      opponent: {
+        deck: new ArrayOfCards([...this.state[stateKeys.opponent.deck]]),
+        discard: new ArrayOfCards([...this.state[stateKeys.opponent.discard]]),
+        banish: new ArrayOfCards([...this.state[stateKeys.opponent.banish]])
+      }
+    };
 
-    // create copies of state
-    let playerDeck = [...(card.player === 'you' ? yourDeck : enemyDeck)];
-    let playerDiscard = [...(card.player === 'you' ? yourDiscard : enemyDiscard)];
-    let playerBanish = [...(card.player === 'you' ? yourBanish : enemyBanish)];
-    let opponentDeck = [...(card.player === 'you' ? enemyDeck : yourDeck)];
-    let opponentDiscard = [...(card.player === 'you' ? enemyDiscard : yourDiscard)];
-    let opponentBanish = [...(card.player === 'you' ? enemyBanish : yourBanish)];
-
-    // define relative setters
-    let setPlayerDeck = card.player === 'you' ? setYourDeck : setEnemyDeck;
-    let setPlayerDiscard = card.player === 'you' ? setYourDiscard : setEnemyDiscard;
-    let setPlayerBanish = card.player === 'you' ? setYourBanish : setEnemyBanish;
-    let setOpponentDeck = card.player === 'you' ? setEnemyDeck : setYourDeck;
-    let setOpponentDiscard = card.player === 'you' ? setEnemyDiscard : setYourDiscard;
-    let setOpponentBanish = card.player === 'you' ? setEnemyBanish : setYourBanish;
+    let activePlayer = card.player === 'you' ? 'player' : 'opponent';
+    let activeOpponent = card.player === 'you' ? 'opponent' : 'player';
 
     const actions = [];
+    const logs = [];
+
+    console.log('playing card', card);
+    this.setState({ isAnimating: true });
 
     const rotatePerspective = () => {
-      let temp;
-      temp = opponentDeck; opponentDeck = playerDeck; playerDeck = temp;
-      temp = opponentDiscard; opponentDiscard = playerDiscard; playerDiscard = temp;
-      temp = opponentBanish; opponentBanish = playerBanish; playerBanish = temp;
-      temp = setOpponentDeck; setOpponentDeck = setPlayerDeck; setPlayerDeck = temp;
-      temp = setOpponentDiscard; setOpponentDiscard = setPlayerDiscard; setPlayerDiscard = temp;
-      temp = setOpponentBanish; setOpponentBanish = setPlayerBanish; setPlayerBanish = temp;
+      const temp = activeOpponent;
+      activeOpponent = activePlayer;
+      activePlayer = temp;
     };
 
     const generateActionsForCard = (card) => {
@@ -91,27 +119,25 @@ export const Clash = () => {
       const attack = card.attack;
       if (attack) {
         for (let i = 0; i < attack; i++) {
-          if (!opponentDeck.length) {
+          if (!stateCopy[activeOpponent].deck.cards.length) {
             break;
           }
 
-          const removedCard = deckUtils.getTopCard(opponentDeck);
-          opponentDeck = deckUtils.removeTopCard(opponentDeck);
-          opponentDiscard = deckUtils.addCardToTop(opponentDiscard, removedCard);
+          const removedCard = stateCopy[activeOpponent].deck.removeTopCard();
+          stateCopy[activeOpponent].discard.addCardToTop(removedCard);
 
+          logs.push('hitting opponent for 1');
           actions.push([
             {
-              pile: opponentDeck,
-              setter: setOpponentDeck,
-              message: 'hitting opponent for 1 (deck -1)'
+              pile: [...stateCopy[activeOpponent].deck.cards],
+              stateKey: stateKeys[activeOpponent].deck
             },
             {
-              pile: opponentDiscard,
-              setter: setOpponentDiscard,
-              message: 'hitting opponent for 1 (discard +1)'
+              pile: [...stateCopy[activeOpponent].discard.cards],
+              stateKey: stateKeys[activeOpponent].discard
             }
           ]);
-          console.log('removed card', removedCard);
+
           if (removedCard.onDiscard) {
             const mockCard = createCard({
               ...removedCard.onDiscard,
@@ -129,24 +155,22 @@ export const Clash = () => {
       const { heal } = card;
       if (heal) {
         for (let i = 0; i < heal; i++) {
-          if (!playerDiscard.length) {
+          if (!stateCopy[activePlayer].discard.cards.length) {
             break;
           }
 
-          const healedCard = deckUtils.getTopCard(playerDiscard);
-          playerDeck = deckUtils.addCardToTop(playerDeck, healedCard);
-          playerDiscard = deckUtils.removeTopCard(playerDiscard);
+          const healedCard = stateCopy[activePlayer].discard.removeTopCard();
+          stateCopy[activePlayer].deck.addCardToTop(healedCard);
 
+          logs.push('healing opponent for 1');
           actions.push([
             {
-              pile: playerDeck,
-              setter: setPlayerDeck,
-              message: 'healing opponent for 1 (deck -1)'
+              pile: [...stateCopy[activePlayer].deck.cards],
+              stateKey: stateKeys[activePlayer].deck
             },
             {
-              pile: playerDiscard,
-              setter: setPlayerDiscard,
-              message: 'hitting opponent for 1 (discard +1)'
+              pile: [...stateCopy[activePlayer].discard.cards],
+              stateKey: stateKeys[activePlayer].discard
             }
           ]);
         }
@@ -155,52 +179,70 @@ export const Clash = () => {
 
     generateActionsForCard(card);
     console.log('actions=', actions);
+    logs.forEach(message => {
+      console.log(message);
+    });
 
     if (actions.length) {
       let i = 0;
-      interval = setInterval(() => {
+      this.interval = setInterval(() => {
         actions[i].forEach(action => {
-          const { pile, setter, message } = action;
-          console.log(message);
-          setter(pile);
+          this.setState({ [action.stateKey]: action.pile });
         });
 
         if (++i === actions.length) {
-          setIsAnimating(false);
-          clearInterval(interval);
+          this.setState({ isAnimating: false });
+          clearInterval(this.interval);
         }
       }, 500);
     } else {
-      setIsAnimating(false);
+      this.setState({ isAnimating: false });
     }
-  };
+  }
 
-  return (
-    <section css={clashCss}>
-      <Card cardProps={cards['Strike']} renderProps={{ x: 310, y: 5 }} />
+  render() {
+    const {
+      yourDeck,
+      yourDiscard,
+      yourBanish,
+      enemyDeck,
+      enemyDiscard,
+      enemyBanish,
+      yourHand,
+      stack
+    } = this.state;
 
-      <div style={{ perspective: '1600px' }}>
-        <EnemyDeck cards={enemyDeck} />
-        <EnemyDiscard cards={enemyDiscard} />
-        <EnemyBanish cards={enemyBanish} />
-      </div>
+    return (
+      <section css={clashCss}>
+        {stack.map((card, index) => (
+          <Card
+            key={index}
+            cardProps={card}
+            renderProps={{ x: 310 + index * 5, y: 5 + index * 5 }}
+          />
+        ))}
 
-      {yourHand.map((card, index) => (
-        <Card
-          key={index}
-          cardProps={card}
-          renderProps={{ x: 175 + 135 * index, y: 280 }}
-          onClick={() => playCard(card, isPlayersTurn)}
-        />
-      ))}
+        <div className='enemy_side'>
+          <EnemyDeck cards={enemyDeck} />
+          <EnemyDiscard cards={enemyDiscard} />
+          <EnemyBanish cards={enemyBanish} />
+        </div>
 
-      <div style={{ perspective: '1600px' }}>
-        <YourDeck cards={yourDeck} />
-        <YourDiscard cards={yourDiscard} />
-        <YourBanish cards={yourBanish} />
-      </div>
+        {yourHand.map((card, index) => (
+          <Card
+            key={index}
+            cardProps={card}
+            renderProps={{ x: 175 + 135 * index, y: 280 }}
+            onClick={() => this.playCard(card)}
+          />
+        ))}
 
-
-    </section>
-  );
-};
+        <div className='your_side'>
+          <YourDeck cards={yourDeck} />
+          <YourDiscard cards={yourDiscard} />
+          <YourBanish cards={yourBanish} />
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/components/clash/arrayOfCards.js
@@ -0,0 +1,18 @@
+export class ArrayOfCards {
+  constructor(cards) {
+    this.cards = cards;
+  }
+
+  getTopCard() {
+    return this.cards[this.cards.length - 1];
+  }
+
+  removeTopCard() {
+    return this.cards.pop();
+  }
+
+  addCardToTop(card) {
+    this.cards.push(card);
+    return this.cards;
+  }
+}
